117ab49a021b0fd58f283b065f9863f080a9fac3
SaraMisajlovska <smisajlovska@gmail.com>
2022-04-08 17:28:16 +0200

    baseUrl to backend updated

--- src/customAxios/axios.js
+++ src/customAxios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8080/api',
+    baseURL: 'https://library-backend-191517.herokuapp.com/api',
     headers: {
         'Access-Control-Allow-Origin': '*'
     }
